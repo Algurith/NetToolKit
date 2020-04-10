fdf53e3205b1c4c0e6325f23c863384900a05bd9
hejinxiong <hejinxiong@flytexpress.com>
2020-04-10 18:22:04 +0800

    Create MemoryCacheHelper

---
+++ ExcelHelperTests/MemoryCacheHelperTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ExcelHelperTests
+{
+    [TestClass()]
+    public class MemoryCacheHelperTests
+    {
+
+        [TestMethod()]
+        public void GetOrSetCacheItemTest() 
+        {
+            var key1 = "key1";
+            var key2 = "key2";
+            var slidingExpiration = new TimeSpan(0, 3, 0);
+            var data1 = MemoryCacheHelper.MemoryCacheHelper.GetOrSetCacheItem(key1, CreateData);
+            var data2 = MemoryCacheHelper.MemoryCacheHelper.GetOrSetCacheItem(key2, CreateData, slidingExpiration);
+
+            //GetOrRemoveCacheItemTest();
+            //Console.WriteLine($"count1：{data1.Count}， count2：{data2.Count}");
+            //for (var i = 0; i <= 10; i++) 
+            //{
+            //    System.Threading.Thread.Sleep(5000);
+            //}
+            //data1 = MemoryCacheHelper.MemoryCacheHelper.GetOrSetCacheItem(key1, CreateData);
+            //data2 = MemoryCacheHelper.MemoryCacheHelper.GetOrSetCacheItem(key2, CreateData, slidingExpiration);
+            //Console.WriteLine($"count1：{data1.Count}， count2：{data2.Count}");
+        }
+
+        private List<string> CreateData() 
+        {
+            return new List<string> { "key1", "key2" };
+        }
+
+        [TestMethod()]
+        public void GetOrRemoveCacheItemTest()
+        {
+            var key = "key2";
+
+            var data = MemoryCacheHelper.MemoryCacheHelper.GetOrRemoveCacheItem<List<string>>(key);
+            Console.WriteLine($"count1：{data.Count}");
+        }
+    }
+}
---
+++ MemoryCacheHelper/MemoryCacheHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace MemoryCacheHelper
+{
+    /// <summary>
+    /// 基于Memory的缓存机制
+    /// </summary>
+    public class MemoryCacheHelper
+    {
+        private static readonly object _addLocker = new object();
+
+        /// <summary>
+        /// 获取或添加缓存节点
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="cachePopulate"></param>
+        /// <returns></returns>
+        public static T GetOrSetCacheItem<T>(string key, Func<T> cachePopulate) 
+        {
+            var slidingExpiration = new TimeSpan(0, 2, 0);
+
+            return GetOrSetCacheItem(key, cachePopulate, slidingExpiration);
+        }
+
+        /// <summary>
+        /// 获取或添加缓存节点
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="cachePopulate"></param>
+        /// <param name="slidingExpiration"></param>
+        /// <param name="absoluteExpiration"></param>
+        /// <returns></returns>
+        public static T GetOrSetCacheItem<T>(string key, Func<T> cachePopulate, TimeSpan slidingExpiration, DateTimeOffset? absoluteExpiration = null)
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentException("Invalid cache key");
+            if (cachePopulate == null) throw new ArgumentNullException("cachePopulate");
+            if (slidingExpiration == null)
+                throw new ArgumentException("Eiter a sliding expiration must be provider");
+
+            if (MemoryCache.Default[key] == null)
+            {
+                lock (_addLocker)
+                {
+                    if (MemoryCache.Default[key] == null)
+                    {
+                        var cacheItem = new CacheItem(key, cachePopulate());
+                        var policy = new CacheItemPolicy
+                        {
+                            SlidingExpiration = slidingExpiration, //指定时长未被访问
+                            //AbsoluteExpiration = absoluteExpiration.Value, //过期时间
+                        };
+                        MemoryCache.Default.Add(cacheItem, policy);
+                    }
+                }
+            }
+
+            return (T)MemoryCache.Default[key];
+        }
+
+        public static T GetOrRemoveCacheItem<T>(string key) 
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentException("Invalid cache key");
+
+            return (T)MemoryCache.Default[key];
+        }
+    }
+}
